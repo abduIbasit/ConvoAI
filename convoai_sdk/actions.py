d945115ae3078a686436ec9bd4e357732cd3588d
Abdulbasit Ayinla <abdulbasitayinla@TISL02.local>
2023-11-04 17:05:13 +0100

    created load_conversation method to retrieve conversation history

--- convoai_sdk/actions.py
+++ convoai_sdk/actions.py
@@ -1,12 +1,38 @@
+import json
+from typing import Text, List, Dict, Tuple, Any
 from .entity_extractor import EntityExtractor
-from ..core import ConvoAI
+# from core import ConvoAI
 
-instance = ConvoAI()
+# instance = ConvoAI()
+# instance.main()
 entity_extractor = EntityExtractor()
 
 slots = {}
 
-def SlotSet(slot_name, value):
+def load_conversation():
+        try:
+            with open("../conversation/conversation.json", "r") as file:
+                lines = file.readlines()
+                if lines:
+                    last_entry = json.loads(lines[-1])
+                    message = last_entry['text']
+                    return message
+                else:
+                    message = None
+        except FileNotFoundError:
+            message = None
+
+def SlotSet(slot_name: Text, value: Any):
+    '''
+    SlotSet method sets slot value or update current value of slot specified
+
+    Parameters:
+    slot_name (Text): The name of the slot to be updated.
+    value (Any): The value to be set for the slot.
+
+    Returns:
+    (None) -> Sets the value of the slot
+    '''
     try:
         if not slot_name or not isinstance(slot_name, str):
             raise ValueError("Slot name must be a non-empty string.")
@@ -16,16 +42,18 @@ def SlotSet(slot_name, value):
     except Exception as e:
         raise ValueError(f"Error setting slot value: {e}")
 
-def get_entities(entity_label):
+def get_entities(entity_label: Text):
     try:
         if not entity_label or not isinstance(entity_label, str):
             raise ValueError("Entity label must be a non-empty string.")
         
-        print(instance.current_question)
-        if instance.current_question is None:
-            raise ValueError("User question could not be retrieved")
+        # print(instance.current_question)
+        # instance.main()
+        if load_conversation == None:
+            raise ValueError("User input could not be retrieved")
         
-        user_question = instance.current_question
+        user_question = load_conversation()
+        print(user_question)
         entity = entity_extractor.get_entities(text=user_question, label=entity_label)
         
         if entity:
@@ -35,7 +63,7 @@ def get_entities(entity_label):
     except Exception as e:
         raise ValueError(f"Error retrieving entities of type {entity_label}: {e}")
 
-def get_slot(slot_name):
+def get_slot(slot_name: Text):
     try:
         if not slot_name in slots:
             raise ValueError(f"Slot '{slot_name}' does not exist.")
