978e9617382cc888c55b7112556ba8525fefd8c9
Abdulbasit Ayinla <abdulbasitayinla@TISL02.local>
2023-11-09 17:57:02 +0100

    modified actions.py

--- convoai_sdk/actions.py
+++ convoai_sdk/actions.py
@@ -1,6 +1,6 @@
 import os
 import json
-from typing import Text, List, Dict, Tuple, Any
+from typing import Text, List, Dict, Tuple, Any, Generator
 from .entity_extractor import EntityExtractor
 # from entity_extractor import EntityExtractor
 
@@ -9,6 +9,20 @@ slots = {}
 
 script_directory = os.path.dirname(os.path.realpath(__file__))
 conversation_directory = os.path.join(os.path.dirname(script_directory), "conversation", "conversation.json")
+prompts_directory = os.path.join(os.path.dirname(script_directory), "conversation", "prompts.txt")
+
+def load_prompts():
+        try:
+            with open(prompts_directory, "r") as file:
+                lines = file.readlines()
+                if lines:
+                    last_entry = (lines[-1])
+                    # message = last_entry['text']
+                    return last_entry
+                else:
+                    return f'Could not retrieve last message'
+        except FileNotFoundError as e:
+            return (f'{e}')
 
 def load_conversation():
         try:
@@ -43,74 +57,30 @@ def SlotSet(slot_name: Text, value: Any):
     except Exception as e:
         raise ValueError(f"Error setting slot value: {e}")
 
-def get_entities(entity_label: Text):
-    try:
-        if not entity_label or not isinstance(entity_label, str):
-            raise ValueError("Entity label must be a non-empty string.")
-        
-        # print(instance.current_question)
-        # instance.main()
-        # if load_conversation == None:
-            # raise ValueError("User input could not be retrieved")
-        
-        user_question = load_conversation()
-        print(user_question)
-        entity = entity_extractor.get_entities(text=user_question, entity_label=entity_label)
-        
-        if entity:
-            return entity
-        else:
-            return None
-    except Exception as e:
-        raise ValueError(f"Error retrieving entities of type {entity_label}: {e}")
+class Tracker:
+    @staticmethod
+    def get_entities(entity_label: Text) -> Generator:
+        try:
+            if not entity_label or not isinstance(entity_label, str):
+                raise ValueError("Entity label must be a non-empty string.")
+            
+            user_question = load_prompts()
+            entity = entity_extractor.get_entities(text=user_question, entity_label=entity_label)
+            
+            if entity:
+                return entity
+            else:
+                list = [None]
+                skip = (l for l in list)
+                return skip
+        except Exception as e:
+            raise ValueError(f"Error retrieving entities of type {entity_label}: {e}")
 
-def get_slot(slot_name: Text):
-    try:
-        if not slot_name in slots:
-            raise ValueError(f"Slot '{slot_name}' does not exist.")
-        return slots.get(slot_name)
-    except Exception as e:
-        raise ValueError(f"Error retrieving slot value: {e}")
-
-
-# import json
-
-# def load_conversation():
-#         try:
-#             with open("../conversation/conversation.json", "r") as file:
-#                 lines = file.readlines()
-#                 if lines:
-#                     last_entry = json.loads(lines[-1])
-#                     message = last_entry['text']
-#                     print(message)
-#                 else:
-#                     print('Not found')
-#         except FileNotFoundError as e:
-#             print(f'{e}')
-
-# load_conversation()
-
-
-# # def get_entities(entity_label: Text):
-# #     try:
-# #         if not entity_label or not isinstance(entity_label, str):
-# #             raise ValueError("Entity label must be a non-empty string.")
-        
-# #         # print(instance.current_question)
-# #         # instance.main()
-# #         # if load_conversation == None:
-# #         #     raise ValueError("User input could not be retrieved")
-        
-# #         user_question = load_conversation()
-# #         # print(user_question)
-# #         entity = entity_extractor.get_entities(text=user_question, entity_label=entity_label)
-        
-# #         if entity:
-# #             print(entity)
-# #         else:
-# #             return None
-# #     except Exception as e:
-# #         raise ValueError(f"Error retrieving entities of type {entity_label}: {e}")
-
-
-# # get_entities("food")
+    @staticmethod
+    def get_slot(slot_name: Text):
+        try:
+            if not slot_name in slots:
+                raise ValueError(f"Slot '{slot_name}' does not exist.")
+            return slots.get(slot_name)
+        except Exception as e:
+            raise ValueError(f"Error retrieving slot value: {e}")
